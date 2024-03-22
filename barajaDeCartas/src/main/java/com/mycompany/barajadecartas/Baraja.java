02e6677feae5db39b09ca28a135cd4bdb28153aa
castro-victoria <mariavictoriacastro15@gmail.com>
2024-03-22 16:21:59 -0300

    metodos baraja

--- barajaDeCartas/src/main/java/com/mycompany/barajadecartas/Baraja.java
+++ barajaDeCartas/src/main/java/com/mycompany/barajadecartas/Baraja.java
@@ -6,6 +6,25 @@ package com.mycompany.barajadecartas;
  * @author Castro Maria Victoria
  */
 public class Baraja {
-    
+    public static void barajar(){
+
+    }
+    public static void siguienteCarta(){
+        
+    }
+    public static void cartasDisponibles(){
+        
+    }
+    public static void darCartas(){
+        
+    }
+    public static void cartasMonton(){
+        
+    }
+    public static void mostrarBaraja(){
+        
+    }
     
 }
+
+
