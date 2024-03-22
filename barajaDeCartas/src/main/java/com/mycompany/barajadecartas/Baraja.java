ea1b29fad315a5fc8cfbd7b3a0d52f7f5a07a5d3
Erick Bunino <erickbunino@gmail.com>
2024-03-22 16:27:23 -0300

    Casi un atributo ...

--- barajaDeCartas/src/main/java/com/mycompany/barajadecartas/Baraja.java
+++ barajaDeCartas/src/main/java/com/mycompany/barajadecartas/Baraja.java
@@ -6,7 +6,9 @@ package com.mycompany.barajadecartas;
  * @author Castro Maria Victoria
  */
 public class Baraja {
+        private List [39] carta; 
     public static void barajar(){
+       
 
     }
     public static void siguienteCarta(){
